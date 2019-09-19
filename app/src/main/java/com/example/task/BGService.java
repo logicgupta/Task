aab92f0fe0a0b8e84c32361cdc77e9fcffd4a7f3
Logic <logicgupta9@gmail.com>
2019-09-19 05:35:53 -0700

    Task

---
+++ app/src/main/java/com/example/task/BGService.java
@@ -0,0 +1,34 @@
+package com.example.task;
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+import androidx.annotation.Nullable;
+
+import org.greenrobot.eventbus.EventBus;
+
+
+public class BGService extends Service {
+
+    private String TAG="BSGService";
+
+    public BGService() {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    public void getData(String s){
+        EventBus.getDefault().post(new EventMessage(s+"Hello"));
+    }
+}
+
---
+++ app/src/main/java/com/example/task/EventMessage.java
@@ -0,0 +1,14 @@
+package com.example.task;
+
+public class EventMessage {
+
+    private String notification;
+
+    public EventMessage(String notification) {
+        this.notification = notification;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+}
